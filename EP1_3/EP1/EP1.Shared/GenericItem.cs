d6abb1f04c2ed7a3e661c09278e2aa6705c40b8c
Chubosaurus <dnguyen_wsu@hotmail.com>
2016-05-05 06:02:06 -0700

    added EP1-EP8

---
+++ EP1_3/EP1/EP1.Shared/GenericItem.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+using Microsoft.Graphics.Canvas;
+using System.Numerics;
+using Windows.Foundation;
+
+
+namespace Chubosaurus
+{
+    public class GenericItem
+    {
+        public GenericItem(string name = "")
+        {
+            this.Name = name;
+
+            this.DrawBoundingRectangle = false;
+        }
+
+        /// <summary>
+        /// Update the GenericItem.
+        /// </summary>
+        /// <param name="dt">A delta time since the last update was called.</param>
+        public virtual void Update(TimeSpan dt)
+        {
+        }
+
+        /// <summary>
+        /// Draw the GenericItem.
+        /// </summary>
+        /// <param name="cds">A surface to draw on.</param>
+        public virtual void Draw(CanvasDrawingSession cds)
+        {
+            if (Bitmap == null)
+                return;
+
+            cds.DrawImage(Bitmap, Location);
+
+            // draw bounding rectangle
+            if(DrawBoundingRectangle)
+                cds.DrawRectangle(BoundingRectangle, Windows.UI.Colors.Purple);
+        }
+
+        public bool SetBitmapFromImageDictionary(string key)
+        {
+            CanvasBitmap cb = null;
+            if (ContentPipeline_Image.ImageDictionary.TryGetValue(key, out cb))
+            {
+                this.Bitmap = cb;
+                this.Size = this.Bitmap.SizeInPixels;
+                return true;
+            }
+
+            return false;
+        }
+
+        // Properties        
+        public string Name { get; set; }
+        public Vector2 Location { get; set; }
+        public CanvasBitmap Bitmap { get; set; }
+        public BitmapSize Size { get; set; }
+        public Rect BoundingRectangle{ get { return new Rect(Location.X, Location.Y, Size.Width - 1, Size.Height - 1); } }
+        public bool DrawBoundingRectangle { get; set; }
+    }
+}
---
+++ EP1_3/EP1/EP1.Windows/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+
+using Microsoft.Graphics.Canvas;
+using Chubosaurus;
+
+namespace EP1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public bool IsAllImagesLoaded = false;
+
+        List<GenericItem> objects = new List<GenericItem>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// CreateResources is where we load in assets.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private async void myDrawingSurface_CreateResources(Microsoft.Graphics.Canvas.CanvasControl sender, Microsoft.Graphics.Canvas.CanvasCreateResourcesEventArgs args)
+        {
+            // TODO: load in assets
+            // set parent canvas
+            ContentPipeline_Image.ParentCanvas = sender;
+            await ContentPipeline_Image.AddImage("cannon", @"Assets/tm_cannon.png");
+            await ContentPipeline_Image.AddImage("clone", @"Assets/tm_clone.png");
+
+
+            Random r = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                
+                GenericItem gi = new GenericItem("test");
+                gi.Location = new System.Numerics.Vector2(r.Next(0, 1000), r.Next(0, 800));
+                gi.SetBitmapFromImageDictionary("cannon");
+                objects.Add(gi);
+            }
+
+
+            IsAllImagesLoaded = true;
+            sender.Invalidate();
+        }
+
+        /// <summary>
+        /// Our Draw function.
+        /// </summary>
+        /// <param name="sender">A Canvas Control.</param>
+        /// <param name="args">Drawing Arguments.</param>
+        private void myDrawingSurface_Draw(Microsoft.Graphics.Canvas.CanvasControl sender, Microsoft.Graphics.Canvas.CanvasDrawEventArgs args)
+        {
+            // get the drawing session
+            CanvasDrawingSession cds = args.DrawingSession;
+
+            // wait until all images are loaded before drawing
+            if (IsAllImagesLoaded)
+            {
+                foreach (GenericItem gi in objects)
+                {
+                    gi.Draw(cds);
+                }
+            }
+        }
+    }
+}
