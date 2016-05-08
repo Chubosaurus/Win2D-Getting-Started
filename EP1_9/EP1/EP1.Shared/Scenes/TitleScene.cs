1aac59193292fb4288923e4a400a73f4562d6461
Chubosaurus <dnguyen_wsu@hotmail.com>
2016-05-08 03:34:10 -0700

    Add EP9 (Generic Scene Switcher)

---
+++ EP1_9/EP1/EP1.Shared/Scenes/TitleScene.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Numerics;
+
+using Windows.UI;
+
+using Microsoft.Graphics.Canvas;
+
+namespace Chubosaurus
+{
+    /// <summary>
+    /// A sample title scene.
+    /// </summary>
+    public class TitleScene : GenericScene
+    {
+        public TitleScene(int width, int height) : base("Generic Title Scene")
+        {
+            this._width = width;
+            this._height = height;
+            SetupScene();
+        }
+
+        public override void Update(TimeSpan dt, GenericInput input)
+        {
+            base.Update(dt, input);
+        }
+
+        public override void Draw(CanvasDrawingSession cds)
+        {
+            base.Draw(cds);
+        }
+
+        protected override void SetupScene()
+        {
+            // NOTE(duan): design the scene manually, wouldn't be great if we had a tool ... lol            
+            _title_label = new CGL_Label("Generic Title Scene", Colors.White, (uint)(this._width * 0.90f), 100);            
+            _title_label.Y = 20;
+            _title_label.FontSize = 50;
+            CenterObject(_title_label, true, false);
+
+
+            _start_button = new CGL_Button("Start A New Game", Colors.White, 350, 50);
+            
+            _top_score_button = new CGL_Button("View The Current Top Scores", Colors.White, 350, 50);
+            _credits_button = new CGL_Button("Credits", Colors.DarkGray, 350, 50);
+
+            CenterObject(_start_button);
+            _top_score_button.Location = new Vector2(_start_button.Location.X, _start_button.Location.Y + _start_button.Size.Height + 10);
+            _credits_button.Location = new Vector2(_top_score_button.Location.X, _top_score_button.Location.Y + _top_score_button.Size.Height + 10);
+
+            this.AddObject(_title_label);
+            this.AddObject(_start_button);
+            this.AddObject(_top_score_button);
+            this.AddObject(_credits_button);
+
+
+            // event callbacks
+            _start_button.ButtonClick += _start_button_ButtonClick;
+            _top_score_button.ButtonClick += _top_score_button_ButtonClick;
+            _credits_button.ButtonClick += _credits_button_ButtonClick;
+        }
+
+        void _credits_button_ButtonClick(object sender, CGL_Button_Event e)
+        {
+            // create the scene switch message to switch the current scene to the credits scene
+            Message_SceneSwitch mss = new Message_SceneSwitch("Credits Scene");
+            InputManager.AddInputItem(mss);            
+        }
+        void _top_score_button_ButtonClick(object sender, CGL_Button_Event e)
+        {
+            // create the scene switch message to switch the current scene to the top score scene
+            Message_SceneSwitch mss = new Message_SceneSwitch("Top Score Scene");
+            InputManager.AddInputItem(mss);            
+        }
+        private void _start_button_ButtonClick(object sender, CGL_Button_Event e)
+        {
+            // create the scene switch message to switch the current scene to the main game scene
+            Message_SceneSwitch mss = new Message_SceneSwitch("Main Game Scene");
+            InputManager.AddInputItem(mss);            
+        }
+
+        private CGL_Label _title_label;
+        private CGL_Button _start_button;
+        private CGL_Button _top_score_button;
+        private CGL_Button _credits_button;
+    }
+}
---
+++ EP1_9/EP1/EP1.Shared/Scenes/TopScoresScene.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+using Windows.UI;
+using Microsoft.Graphics.Canvas;
+
+namespace Chubosaurus
+{
+    /// <summary>
+    /// A sample Top Score Scene.
+    /// </summary>
+    public class TopScoresScene : GenericScene
+    {
+        public TopScoresScene(int width, int height)
+            : base("Top Score Scene")
+        {
+            this._width = width;
+            this._height = height;
+            this.SetupScene();
+        }
+
+        protected override void SetupScene()
+        {
+            // NOTE(duan): design the scene manually, wouldn't be great if we had a tool ... lol            
+            _title_label = new CGL_Label("Generic Top Score Scene", Colors.White, (uint)(this._width * 0.60f), 100);
+            _title_label.Y = 20;
+            _title_label.FontSize = 50;
+            CenterObject(_title_label, true, false);
+
+            _back_button = new CGL_Button("< Back", Colors.White);
+            _back_button.SetSize(100, 100);
+            _back_button.Location = new Vector2(_title_label.Location.X - _back_button.Size.Width - 10, _title_label.Location.Y);
+
+            this.AddObject(_title_label);
+            this.AddObject(_back_button);
+
+            // event call backs
+            _back_button.ButtonClick += _back_button_ButtonClick;
+        }
+
+        void _back_button_ButtonClick(object sender, CGL_Button_Event e)
+        {
+            // create a switch message that pop that scene_history
+            Message_GoBack mgb = new Message_GoBack();
+            InputManager.AddInputItem(mgb);
+        }
+
+        private CGL_Label _title_label;
+        private CGL_Button _back_button;
+    }
+}
