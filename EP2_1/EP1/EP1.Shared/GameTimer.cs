ad4bb6d9455930e8807464a0c4f0598589694d64
Chubosaurus <dnguyen_wsu@hotmail.com>
2016-05-08 05:16:36 -0700

    Add EP2_1 (Generic Sprite Sheet Animation)

---
+++ EP2_1/EP1/EP1.Shared/GameTimer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Graphics.Canvas;        // CanvasControl (Win2D)
+using Windows.UI.Xaml;                  // DispatchTimer
+
+namespace Chubosaurus
+{
+    /// <summary>
+    /// The timer that dictates the Game Loop.
+    /// </summary>
+    public class GameTimer
+    {
+        /// <summary>
+        /// GameTimer constructor, sets default values.
+        /// </summary>
+        public GameTimer(CanvasControl surface = null, int UPS = 60, int FPS = 30)
+        {
+            UpdateTimer = new DispatcherTimer();
+            UpdateTimer.Interval = TimeSpan.FromMilliseconds(1000 / UPS);        // 60 UPS
+            UpdateTimer.Tick += UpdateTimer_Tick;
+
+            // set FPS
+            TimeBetweenDraw = TimeSpan.FromMilliseconds(1000 / FPS);             // default 30 fps
+
+            LastUpdateTime = DateTime.Now;
+            LastDrawTime = DateTime.Now;
+            TotalAppTime = TimeSpan.FromMilliseconds(0);
+
+            // set the canvas control
+            ParentCanvas = surface;
+
+            UpdateTimer.Start();
+        }
+
+        /// <summary>
+        /// Update timer for the game, this should run 60 times a second (default).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void UpdateTimer_Tick(object sender, object e)
+        {
+            if (!IsUpating)
+            {
+                IsUpating = true;
+
+                // compute the delta time
+                TimeSpan dt = DateTime.Now - LastUpdateTime;
+
+                // update the last time the program has updated
+                LastUpdateTime = DateTime.Now;
+
+                // update the total app time
+                TotalAppTime += dt;
+
+                // get the current item from the InputManager
+                GenericInput gi = InputManager.Update();
+
+                // NOTE(duan): we need to update the STORYBOARD
+                if (SceneManager.Update(dt, gi))
+                {
+                    // NOTE(duan): do nothing in the game loop since we're switching scenes or going back in the history stack.                    
+                }
+                else
+                {
+                    // update the current scene
+                    if (SceneManager.CurrentScene != null)
+                    {
+                        if (gi is MouseGenericInput)
+                        {
+                            while (gi is MouseGenericInput)
+                            {
+                                SceneManager.CurrentScene.Update(dt, gi);
+                                gi = InputManager.PeekAndTake(typeof(MouseGenericInput));
+                            }
+                        }
+                        else
+                        {
+                            SceneManager.CurrentScene.Update(dt, gi);
+                        }
+                    }
+                }
+
+                // figure out if we need to draw/refresh the screen
+                TimeSpan dt_draw = DateTime.Now - LastDrawTime;
+                if(dt_draw > TimeBetweenDraw)
+                {
+                    if (ParentCanvas != null)
+                    {
+                        ParentCanvas.Invalidate();
+                        LastDrawTime = DateTime.Now;
+                    }
+                }
+
+                IsUpating = false;
+            }
+        }
+
+        // Properties
+
+        /// <summary>
+        /// The UpdateTimer (for now it is a DispatchTimer)
+        /// </summary>
+        private DispatcherTimer UpdateTimer;
+
+        /// <summary>
+        /// Update() flag.  If this is set the Update() function is running.  This is to prevent another call to Update() while one is already running.
+        /// </summary>
+        public bool IsUpating = false;
+
+        /// <summary>
+        /// Total running time of the Application.
+        /// </summary>
+        public TimeSpan TotalAppTime { get; set; }
+
+        /// <summary>
+        /// The last time the Update was call.
+        /// </summary>
+        public DateTime LastUpdateTime { get; set; }
+
+        /// <summary>
+        /// The last time the Draw was call.
+        /// </summary>
+        public DateTime LastDrawTime { get; set; }
+
+        /// <summary>
+        /// The delta time between Draw calls.  (This is basically your FPS)
+        /// </summary>
+        public TimeSpan TimeBetweenDraw { get; set; }
+
+        /// <summary>
+        /// Pointer to the current CanvasControl.  This is needed so we can Invalidate() the surface to cause a Redraw.
+        /// </summary>
+        public CanvasControl ParentCanvas { get; set; }        
+    }
+}
---
+++ EP2_1/EP1/EP1.Shared/Scenes/TitleScene.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Numerics;
+
+using Windows.UI;
+
+using Microsoft.Graphics.Canvas;
+
+namespace Chubosaurus
+{
+    /// <summary>
+    /// A sample title scene.
+    /// </summary>
+    public class TitleScene : GenericScene
+    {
+        public TitleScene(int width, int height) : base("Generic Title Scene")
+        {
+            this._width = width;
+            this._height = height;
+            SetupScene();
+        }
+
+        public override void Update(TimeSpan dt, GenericInput input)
+        {
+            base.Update(dt, input);
+        }
+
+        public override void Draw(CanvasDrawingSession cds)
+        {
+            base.Draw(cds);
+        }
+
+        protected override void SetupScene()
+        {
+            // NOTE(duan): design the scene manually, wouldn't be great if we had a tool ... lol            
+            _title_label = new CGL_Label("Generic Title Scene", Colors.White, (uint)(this._width * 0.90f), 100);            
+            _title_label.Y = 20;
+            _title_label.FontSize = 50;
+            CenterObject(_title_label, true, false);
+
+            _start_button = new CGL_Button("Start A New Game", Colors.White, 350, 50);            
+            _top_score_button = new CGL_Button("View The Current Top Scores", Colors.White, 350, 50);
+            _credits_button = new CGL_Button("Credits", Colors.DarkGray, 350, 50);
+            _animation_button = new CGL_Button("Play | Stop Animation", Colors.Green, 350, 50);
+
+            CenterObject(_start_button);
+            _top_score_button.Location = new Vector2(_start_button.Location.X, _start_button.Location.Y + _start_button.Size.Height + 10);
+            _credits_button.Location = new Vector2(_top_score_button.Location.X, _top_score_button.Location.Y + _top_score_button.Size.Height + 10);
+            _animation_button.Location = new Vector2(_credits_button.Location.X, _credits_button.Location.Y + _credits_button.Size.Height + 10);
+
+            this.AddObject(_title_label);
+            this.AddObject(_start_button);
+            this.AddObject(_top_score_button);
+            this.AddObject(_credits_button);
+            this.AddObject(_animation_button);
+
+            // event callbacks
+            _start_button.ButtonClick += _start_button_ButtonClick;
+            _top_score_button.ButtonClick += _top_score_button_ButtonClick;
+            _credits_button.ButtonClick += _credits_button_ButtonClick;
+            _animation_button.ButtonClick += _animation_button_ButtonClick;
+
+            // EP2_1 Uncomment if you want to test a single animation
+            //_animation = new GenericSpriteAnimation();
+            //_animation.CutAndInsert("explosion", 64, 64);
+            //CenterObject(_animation, true, false);
+            //_animation.Y = 100;
+            //this.AddObject(_animation);
+
+            // Create 100 explosions
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                GenericSpriteAnimation _animation = new GenericSpriteAnimation();
+                _animation.FramesFromSpriteSheet("explosion", 64, 64);
+                _animation.Location = new Vector2(r.Next(this._width), r.Next(this._height));
+                _animation.AnimationSpeed = r.Next(12, 36);
+                _animation.Play();
+                this.AddObject(_animation);                
+            }
+        }
+
+        void _animation_button_ButtonClick(object sender, CGL_Button_Event e)
+        {
+            // ghetto rig the animation for now
+            foreach (GenericItem gi in objects)
+            {
+                if (gi is GenericSpriteAnimation)
+                {
+                    GenericSpriteAnimation gsa = gi as GenericSpriteAnimation;
+                    if (gsa.IsPlaying)
+                    {
+                        gsa.Stop();
+                    }
+                    else
+                    {
+                        gsa.Play();
+                    }
+                }
+            }
+        }
+
+        void _credits_button_ButtonClick(object sender, CGL_Button_Event e)
+        {
+            // create the scene switch message to switch the current scene to the credits scene
+            Message_SceneSwitch mss = new Message_SceneSwitch("Credits Scene");
+            InputManager.AddInputItem(mss);            
+        }
+        void _top_score_button_ButtonClick(object sender, CGL_Button_Event e)
+        {
+            // create the scene switch message to switch the current scene to the top score scene
+            Message_SceneSwitch mss = new Message_SceneSwitch("Top Score Scene");
+            InputManager.AddInputItem(mss);            
+        }
+        private void _start_button_ButtonClick(object sender, CGL_Button_Event e)
+        {
+            // create the scene switch message to switch the current scene to the main game scene
+            Message_SceneSwitch mss = new Message_SceneSwitch("Main Game Scene");
+            InputManager.AddInputItem(mss);            
+        }
+
+        private CGL_Label _title_label;
+        private CGL_Button _start_button;
+        private CGL_Button _top_score_button;
+        private CGL_Button _credits_button;
+        private CGL_Button _animation_button;
+
+        // EP2_1 Uncomment if you want to test a single animation
+        //private GenericSpriteAnimation _animation;
+    }
+}
